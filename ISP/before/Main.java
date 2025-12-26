df0654081f74d166b9c5f402ba51912e110a6d1e
Sharifatou92 <issharifatou@gmail.com>
2025-12-26 18:11:43 +0100

    ISP - Main avant refactoring

---
+++ ISP/before/Main.java
@@ -0,0 +1,13 @@
+package ict301.solid.isp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Worker human = new Human();
+        human.work();
+        human.eat();
+
+        Worker robot = new Robot();
+        robot.work();
+    }
+}
