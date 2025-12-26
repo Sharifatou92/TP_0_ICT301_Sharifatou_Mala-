a0f7b50322561734f31a88686f1f81f1a856f502
Sharifatou92 <issharifatou@gmail.com>
2025-12-26 18:19:51 +0100

    ISP - Human après refactoring

---
+++ ISP/after/Human.java
@@ -0,0 +1,14 @@
+package ict301.solid.isp;
+
+public class Human implements Workable, Feedable {
+
+    @Override
+    public void work() {
+        System.out.println("Human is working");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Human is eating");
+    }
+}
