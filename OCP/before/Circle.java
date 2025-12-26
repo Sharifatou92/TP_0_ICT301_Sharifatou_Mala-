87fc7112607af832edd7c9e4fe75066794d092f5
Sharifatou92 <issharifatou@gmail.com>
2025-12-26 16:23:40 +0100

    OCP - Circle avant refactoring

---
+++ OCP/before/Circle.java
@@ -0,0 +1,13 @@
+package ict301.solid.ocp;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
