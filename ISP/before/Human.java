dadb5eeb2e0d7cf0adb3799e03bce7bf405745e4
Sharifatou92 <issharifatou@gmail.com>
2025-12-26 18:03:45 +0100

    ISP - Human avant refactoring

---
+++ ISP/before/Human.java
@@ -0,0 +1,14 @@
+package ict301.solid.isp;
+
+public class Human implements Worker {
+
+    @Override
+    public void work() {
+        System.out.println("Human is working");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Human is eating");
+    }
+}
