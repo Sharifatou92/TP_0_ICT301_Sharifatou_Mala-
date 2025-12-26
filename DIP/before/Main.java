808de09ddb449a55edd71ba519a4c0839fb61658
Sharifatou92 <issharifatou@gmail.com>
2025-12-26 19:08:38 +0100

    DIP - Main avant refactoring

---
+++ DIP/before/Main.java
@@ -0,0 +1,9 @@
+package ict301.solid.dip;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserService service = new UserService();
+        service.process();
+    }
+}
