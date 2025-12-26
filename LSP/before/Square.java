0d1eadc7b53d78f9a0ce8bf3b24106a43c77c2e7
Sharifatou92 <issharifatou@gmail.com>
2025-12-26 17:40:03 +0100

    LSP - Square avant refactoring

---
+++ LSP/before/Square.java
@@ -0,0 +1,16 @@
+package ict301.solid.lsp;
+
+public class Square extends Rectangle {
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+        this.height = width;
+    }
+
+    @Override
+    public void setHeight(int height) {
+        this.width = height;
+        this.height = height;
+    }
+}
