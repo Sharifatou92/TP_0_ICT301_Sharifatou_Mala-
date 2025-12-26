2909c1462f65b41521b147b8e90523b389643550
Sharifatou92 <issharifatou@gmail.com>
2025-12-26 17:56:58 +0100

    LSP - Square après refactoring

---
+++ LSP/after/Square.java
@@ -0,0 +1,15 @@
+package ict301.solid.lsp;
+
+public class Square implements Shape {
+
+    private int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    @Override
+    public int getArea() {
+        return side * side;
+    }
+}
